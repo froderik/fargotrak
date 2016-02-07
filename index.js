08307b12b2a2ceadc4349e2c2b05cc46ce2e1690
Fredrik Rubensson <fredrik@eldfluga.se>
2016-02-07 21:31:59 +0100

    removes nonneeded code from the scaffold

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 var self = require("sdk/self");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
-
-exports.dummy = dummy;
-
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 
