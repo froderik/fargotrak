fabbfa97152c4c07614da7375662d473afadbc4a
Fredrik Rubensson <fredrik@eldfluga.se>
2016-02-07 22:00:47 +0100

    a popup form that posts into a blck whole - a start at least

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var self = require("sdk/self");
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 
+var panel = require("sdk/panel").Panel({
+    contentURL: self.data.url("form.html")
+});
+
 var button = buttons.ActionButton({
   id: "mozilla-link",
   label: "Visit Mozilla",
@@ -15,5 +19,5 @@ var button = buttons.ActionButton({
 });
 
 function handleClick(state) {
-  tabs.open("https://www.mozilla.org/");
+    panel.show();
 }
